22cf9aa6808a44e2c71db3e53fe47f3b1aa1ee99
Jesper <evilj84@gmail.com>
2013-09-18 11:59:20 +0200

    Hej hej

--- TreatPraktik/TreatPraktik/Model/WorkSheets/WorkSheetktResources.cs
+++ TreatPraktik/TreatPraktik/Model/WorkSheets/WorkSheetktResources.cs
@@ -42,58 +42,20 @@ namespace TreatPraktik.Model
                 //If the cell contains a Shared String, its value will be a 
                 //  reference id which will be used to look up the value in the 
                 //  Shared String table.
-                //IEnumerable<String> textValues =
-                //  from cell in row.Descendants<Cell>()
-                //  where cell.CellValue != null
-                //  select
-                //    (cell.DataType != null
-                //      && cell.DataType.HasValue
-                //      && cell.DataType == CellValues.SharedString
-                //    ? sharedString.ChildElements[
-                //      int.Parse(cell.CellValue.InnerText)].InnerText
-                //    : cell.CellValue.InnerText)
-                //  ;
-
                 IEnumerable<String> textValues =
-  from cell in row.Descendants<Cell>()
-  where cell.CellValue != null
-  select
-    (cell.DataType != null
-      && cell.DataType.HasValue
-      && cell.DataType == CellValues.SharedString
-    ? sharedString.ChildElements[
-      int.Parse(cell.CellValue.InnerText)].InnerText
-    : cell.CellValue.InnerText)
-  ;
+                    from cell in row.Descendants<Cell>()
+                    where cell.CellValue != null
+                    select
+                      (cell.DataType != null
+                        && cell.DataType.HasValue
+                        && cell.DataType == CellValues.SharedString
+                      ? sharedString.ChildElements[
+                        int.Parse(cell.CellValue.InnerText)].InnerText
+                      : cell.CellValue.InnerText);
 
                 //Check to verify the row contained data.
                 if (textValues.Count() > 0)
                 {
-                    //Create a ktExaminedGroup and add it to the list.
-                    //var textArray = textValues.ToArray();
-                    //ktUIDesign design = new ktUIDesign();
-                    //design.DesignID = Int32.Parse(textArray[0]);
-                    //design.DatabaseTableName = textArray[1];
-                    //design.DatabaseFieldName = textArray[2];
-                    //design.CodeTableName = textArray[3];
-                    //design.ResxID = textArray[4];
-                    //design.ReadOnlyPolicy = Int32.Parse(textArray[5]);
-                    //design.InputDataType = Int32.Parse(textArray[6]);
-                    //design.MortyParameter = Int32.Parse(textArray[7]);
-                    //design.RequiredID = Int32.Parse(textArray[8]);
-                    //design.GUIUnitShortName = textArray[9];
-                    //design.DatabaseUnitName = textArray[10];
-                    //design.LabkaUnitName = textArray[11];
-                    //design.DatabaseToUIConversion = textArray[12];
-                    //design.DefaultValue = textArray[13];
-                    //design.NormalRangeMinimum = Int32.Parse(textArray[14]);
-                    //design.NormalRangeMaximum = Int32.Parse(textArray[15]);
-                    //design.CopyEncounter = Int32.Parse(textArray[16]);
-                    //design.CopyEpisode = Int32.Parse(textArray[17]);
-                    //design.DataQualityScore = Int32.Parse(textArray[18]);
-                    //design.CopyFinalEncounter = Int32.Parse(textArray[19]);
-                    //result.Add(design);
-
                     var textArray = textValues.ToArray();
                     ktResources resource = new ktResources();
                     resource.ResourceID = textArray[0];
